e434f7db1b094d7760cc7a54049baab9dbcb1f7d
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2017-02-01 15:30:26 -0700

    add show and year component

--- src/app/Components/app.routes.ts
+++ src/app/Components/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { YearsComponent } from './years/years.component';
 import { ShowsComponent } from './shows/shows.component';
+import { ShowComponent  } from './show/show.component';
+import { YearComponent  } from './year/year.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'years', component: YearsComponent },
-  { path: 'shows', component: ShowsComponent }
+  { path: 'years/:date', component: YearComponent },
+  { path: 'shows', component: ShowsComponent },
+  { path: 'shows/:date', component: ShowComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { YearsComponent } from './Components/years/years.component';
 import { YearBoxComponent } from './Components/year-box/year-box.component';
 import { ShowsComponent } from './Components/shows/shows.component';
 import { ShowBoxComponent } from './Components/show-box/show-box.component';
+import { ShowComponent } from './Components/show/show.component';
+import { YearComponent } from './Components/year/year.component';
 
 import { YearService } from './Services/year.service';
 import { ShowService } from './Services/show.service';
@@ -29,7 +31,9 @@ import { Shader } from './Services/shader';
     YearsComponent,
     YearBoxComponent,
     ShowsComponent,
-    ShowBoxComponent
+    ShowBoxComponent,
+    ShowComponent,
+    YearComponent
   ],
   imports: [
     BrowserModule,
