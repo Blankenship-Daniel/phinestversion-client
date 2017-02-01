71bdfa4681937499bbc0fdbc133aecb791d642a6
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2017-02-01 16:02:35 -0700

    create global_components dir

---
+++ src/app/Components/global_components/heading-panel/heading-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heading-panel',
+  templateUrl: './heading-panel.component.html',
+  styleUrls: ['./heading-panel.component.css']
+})
+export class HeadingPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
