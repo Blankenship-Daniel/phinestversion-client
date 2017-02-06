4d2f985388262b6e12534f47fb5be4b3a55a6a5a
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2017-02-06 15:48:43 -0700

    add links to song and show titles

--- src/app/Components/songs/song/submission-box/submission-box.component.ts
+++ src/app/Components/songs/song/submission-box/submission-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 import { Submission } from '../../../../Models/submission.model';
 import { ShaderService } from '../../../../Services/shader.service';
 import { CommentService } from '../../../../Services/comment.service';
@@ -11,37 +11,46 @@ import { Comment } from '../../../../Models/comment.model';
 })
 export class SubmissionBoxComponent implements OnInit {
 
-    @Input() submission: Submission;
-    @Input() index: number;
-    @Input() songTitle: boolean = false;
-    @Input() showTitle: boolean = false;
+  @Input() submission: Submission;
+  @Input() index: number;
+  @Input() songTitle: boolean = false;
+  @Input() showTitle: boolean = false;
 
-    private comments: Comment[];
-    private showComments: boolean = false;
+  private comments: Comment[];
+  private showComments: boolean = false;
+  private offset: number = 0;
 
-    constructor(
-      private shader: ShaderService,
-      private commentService: CommentService
-    ) { }
+  constructor(
+    private shader: ShaderService,
+    private commentService: CommentService
+  ) { }
 
-    ngOnInit() {
-      this.loadComments();
+  ngOnInit() {
+    this.loadComments();
+  }
+
+  toggleComments() {
+    this.showComments = !this.showComments;
+  }
+
+  getShade(): number {
+    return this.shader.getShade(this.index);
+  }
+
+  addComments(comments: Comment[]) {
+    if (this.comments === undefined) {
+      this.comments = comments;
+      return;
     }
+    this.comments.push.apply(this.comments, comments);
+  }
 
-    toggleComments() {
-      this.showComments = !this.showComments;
-    }
-
-    getShade(): number {
-      return this.shader.getShade(this.index);
-    }
-
-    loadComments() {
-      this.commentService.getCommentsBySubmissionId(this.submission.submission_id, 0, 0).subscribe(
-        comments => this.comments = comments,
-        err      => {
-            console.log(err);
-        }
-      );
-    }
+  loadComments() {
+    this.commentService.getCommentsBySubmissionId(this.submission.submission_id, 20, this.offset).subscribe(
+      comments => this.addComments(comments),
+      err      => {
+          console.log(err);
+      }
+    );
+  }
 }
--- src/app/Models/submission.model.ts
+++ src/app/Models/submission.model.ts
@@ -1,6 +1,7 @@
 export class Submission {
   constructor(
     public song_title         : string,
+    public song_slug          : string,
     public submission_id      : number,
     public description        : string,
     public show_date          : string,
