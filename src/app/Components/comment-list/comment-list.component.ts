14c853c40b9ffff9bb5d0d9918b987f80fc9138e
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2017-03-03 09:25:06 -0700

    add userLocalStorage service to handle interactions with the clientside user object

--- src/app/Components/comment-list/comment-list.component.ts
+++ src/app/Components/comment-list/comment-list.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
-import { CommentService } from '../../Services/comment.service';
 import { Comment } from '../../Models/comment.model';
-
-
+import { CommentService } from '../../Services/comment.service';
 
 @Component({
   selector: 'app-comment-list',
@@ -13,17 +11,53 @@ import { Comment } from '../../Models/comment.model';
 })
 export class CommentListComponent implements OnInit {
 
-  @Input() submissionId: number;
-  @Input() showComments: boolean;
+  /**
+   * A list of Comment's to be displayed corresponding to a given
+   *  submission.
+   * @type {Comment[]}
+   */
   @Input() comments: Comment[];
+
+  /**
+   * Notifies the SubmissionBoxComponent that a comment has been
+   *  submitted.
+   * @type {EventEmitter<boolean>}
+   */
   @Output() commentSubmitted: EventEmitter<boolean>;
 
-  private offset: number = 0;
-  private slug: string;
-  private formSubmitted: boolean;
+  /**
+   * Shows the comments if set to true. Hides the comments if set
+   *  to false.
+   * @type {boolean}
+   */
+  @Input() showComments: boolean;
+
+  /**
+   * The number that corresponds to a submission in the database.
+   * @type {number}
+   */
+  @Input() submissionId: number;
+
+  /**
+   * The form to submit a new comment on a given submission.
+   * @type {FormGroup}
+   */
   private commentForm: FormGroup;
 
+  /**
+   * Keeps track of whether the commentForm has been submitted or not.
+   * @type {boolean} true if the commentForm has been submitted, false
+   *                  false otherwise.
+   */
+  private formSubmitted: boolean;
+
   constructor(
+
+    /**
+     * Handles requests to the API and returns data from the `comments`
+     *  table.
+     * @type {CommentService}
+     */
     private commentService: CommentService
   ) {
     this.formSubmitted = false;
@@ -36,6 +70,14 @@ export class CommentListComponent implements OnInit {
   ngOnInit() {
   }
 
+  /**
+   * Adds comments returned from the commentService and adds
+   *  them to the end of the comments array. If the comments
+   *  array is undefined, it assigns the return value from the
+   *  commentService to the comments array.
+   * @param  {Comment[]} comments an array of comments returned
+   *                               from the submitComment function.
+   */
   addComments(comments: Comment[]) {
     if (this.comments === undefined) {
       this.comments = comments;
@@ -48,11 +90,20 @@ export class CommentListComponent implements OnInit {
     this.commentSubmitted.emit(true);
   }
 
-  submitComment(form, valid) {
+  /**
+   * Passes the contents of the commentForm to the commentService to be
+   *  submitted to the API. Upon success, the data is returned and added
+   *  to the comments array.
+   * @param  {Object}  form  contains the contens of the commentForm.
+   * @param  {boolean} valid true if the contents of the commentForm are valid,
+   *                          false otherwise.
+   */
+  submitComment(form: any, valid: boolean) {
     this.formSubmitted = true;
+
     if (valid) {
-      let user            = JSON.parse(localStorage.getItem('user'));
-      let userId: number  = user.id;
+      let user    : any     = JSON.parse(localStorage.getItem('user'));
+      let userId  : number  = user.id;
 
       // TODO: user error handling
 
---
+++ src/app/Services/userLocalStorage.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class UserLocalStorageService {
+
+  /**
+   * An array of properties that the user object should have.
+   * @type {string[]}
+   */
+  private props: string[];
+
+  /**
+   * An instance of the user object stored in localStorage.
+   * @type {any}
+   */
+  private user: any;
+
+  /**
+   * Checks the user object from localStorage against a set
+   *  of properties that should exist on the object.
+   * @return {boolean} true if the user object from localStorage exists,
+   *                         and all required properties exist on it, false
+   *                         otherwise.
+   */
+  private hasProps() {
+    if (this.user === null) {
+      return false;
+    }
+
+    for (let i: number = 0; i < this.props.length; i++) {
+      if (!this.user.hasOwnProperty(this.props[i]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  constructor() {
+    this.user   = JSON.parse(localStorage.getItem('user'));
+    this.props  = [
+      'id',
+      'image',
+      'username',
+      'email',
+      'created_at',
+      'updated_at'
+    ];
+  }
+
+  public authUser() {
+    return this.hasProps();
+  }
+
+  getUserId() {
+    return this.user.id;
+  }
+
+  getUserName() {
+    return this.user.username;
+  }
+
+  getUserImage() {
+    return this.user.image;
+  }
+
+  redirectToLogin() {
+    location.pathname = '/login';
+  }
+}
