3646fa8b7c5588d49b30baa5e646bba925b74b2d
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2017-03-09 08:39:55 -0700

    add comments to shader service

--- src/app/Services/shader.service.ts
+++ src/app/Services/shader.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ShaderService {
 
+  /**
+   * Allows a shade value to be accessed at a given index.
+   * @type {Object}
+   */
   private shades: Object = {
     0: 700,
     1: 600,
@@ -13,6 +17,12 @@ export class ShaderService {
 
   constructor() { }
 
+  /**
+   * Gets the shade number that corresponds to a color class such as grey700-bg.
+   * @param  {number} index the index number of a given submission. For example, index
+   *                          index of 0 returns the number 700.
+   * @return {number} the shade number.
+   */
   getShade(index: number): number {
     return this.shades.hasOwnProperty(index) ?
            this.shades[index] : 100;
